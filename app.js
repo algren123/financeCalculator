d1c9daaaa7d561bb0d5a1d69b604afba64d8e975
Algren Pauna <algren.pauna@gmail.com>
2020-06-22 22:58:01 +0100

    updates

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-function calcFinance(amount, term, interest, deposit){
+const myForm = document.getElementById('info-form');
 
-    interestPay = (amount-deposit)*(interest/100);
-    monthly = (amount-deposit+interestPay) / term;
-    return monthly;
+myForm.addEventListener("submit", calcFinance);
+
+function calcFinance(e){
+    e.preventDefault();
+
+    const amount = document.getElementById('amount').value;
+    const term = document.getElementById('term').value;
+    const interest = document.getElementById('interest').value;
+    const deposit = document.getElementById('deposit').value;
+
+    let interestPay = (amount-deposit)*(interest/100);
+    let monthly = (amount-deposit+interestPay) / term;
+    
+    document.getElementById('answer').textContent = monthly;
 }
-
-console.log(monthly);
