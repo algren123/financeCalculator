176534769fb70d4a350df14533733a5f4c0a384d
Algren Pauna <algren.pauna@gmail.com>
2020-06-22 17:26:28 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+function calcFinance(amount, term, interest, deposit){
+
+    interestPay = (amount-deposit)*(interest/100);
+    monthly = (amount-deposit+interestPay) / term;
+    return monthly;
+}
