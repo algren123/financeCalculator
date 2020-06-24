c9b93c122b5481b90378d7ffa912cdafae97490c
Algren Pauna <algren.pauna@gmail.com>
2020-06-24 18:19:25 +0100

    added display for interest and added message for invalid information

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const myForm = document.getElementById('info-form');
 
 myForm.addEventListener("submit", calcFinance);
 
+
 function calcFinance(e){
     e.preventDefault();
 
@@ -10,8 +11,19 @@ function calcFinance(e){
     const interest = document.getElementById('interest').value;
     const deposit = document.getElementById('deposit').value;
 
-    let interestPay = (amount-deposit)*(interest/100);
-    let monthly = (amount-deposit+interestPay) / term;
-    
-    document.getElementById('answer').textContent = Math.round(monthly*10)/10;
+        //checks if the amount entered is correct/valid and if it isn't, it lets the user know
+    if (amount <= 0 || term <= 0 || interest < 0 || deposit < 0){
+        document.getElementById('result-id').style.visibility = 'visible';
+        document.getElementById('answer').textContent = 'Please enter valid information';
+    } else {
+        //calculates amount of interest
+        let interestPay = (amount-deposit)*(interest/100);
+        //calculates monthly payments
+        let monthly = (amount-deposit+interestPay) / term;
+
+        
+        document.getElementById('result-id').style.visibility = 'visible';
+        document.getElementById('answer').textContent = 'Result: £' + Math.round(monthly*10)/10 + "/month";
+        document.getElementById('interest-paid').textContent = 'Total interest paid: £' + Math.round(interestPay*10)/10;
+        }
 }
