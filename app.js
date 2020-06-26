e14c5aacb608f76ab504fb333789d588417daf5c
Algren Pauna <algren.pauna@gmail.com>
2020-06-26 14:14:43 +0100

    Added compound interest to work and reworked the code for a better and more accurate result

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ function calcFinance(e){
 
     const amount = document.getElementById('amount').value;
     const term = document.getElementById('term').value;
-    const interest = document.getElementById('interest').value;
+    //const interest = document.getElementById('interest').value;
+    const interest = document.getElementById('interest');
     const deposit = document.getElementById('deposit').value;
 
         //checks if the amount entered is correct/valid and if it isn't, it lets the user know
@@ -17,13 +18,16 @@ function calcFinance(e){
         document.getElementById('answer').textContent = 'Please enter valid information';
     } else {
         //calculates amount of interest
-        let interestPay = (interest/term) * (amount-deposit);
+        //let interestPay = (interest/term) * (amount-deposit);
+        let interestPay = parseFloat(interest.value) / 100 / 12;
+        let power = Math.pow(1 + interestPay, term);
         //calculates monthly payments
-        let monthly = (amount-deposit+interestPay) / term;
+        //let monthly = (amount-deposit+interestPay) / term;
+        let monthly = ((amount-deposit)*power*interestPay)/(power-1);
 
         
         document.getElementById('result-id').style.visibility = 'visible';
         document.getElementById('answer').textContent = 'Result: £' + ((monthly*100)/100).toFixed(2) + "/month";
-        document.getElementById('interest-paid').textContent = 'Total interest paid: £' + ((interestPay*100)/100).toFixed(2);
+        document.getElementById('interest-paid').textContent = 'Total interest paid: £' + ((monthly*term)-(amount-deposit)).toFixed(2);
         }
 }
